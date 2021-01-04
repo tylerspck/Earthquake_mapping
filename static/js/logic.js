6fc23dcbdbbe4ba1c6994e62fbba2d3fbd1559ff
tylerspck <60820827+tylerspck@users.noreply.github.com>
2021-01-04 18:40:48 -0500

    adding readme

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,12 +9,12 @@ function createMap(earthquakes_week, earthquakes_day, earthquakes_hour, heat, pl
         crossOrigin: true,
     });
 
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 256,
         maxZoom: 18,
         // zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "streets-v11",
         accessToken: API_KEY
     });
 
